754d53d782304491ac2a277d8a8b554335c3a00f
Marcus Tesla <katetv2000.mail.ru>
2022-02-24 17:56:26 +0500

    Initial commit

---
+++ app/src/main/java/com/example/text_notes_app/screens/main/Adapter.java
@@ -0,0 +1,98 @@
+package com.example.text_notes_app.screens.main;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SortedList;
+
+import com.example.text_notes_app.R;
+import com.example.text_notes_app.model.Note;
+
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.NoteViewHolder> {
+
+    private SortedList<Note> sortedList;
+    public Adapter() {
+        sortedList = new SortedList<>(Note.class, new SortedList.Callback<Note>() {
+            @Override
+            public int compare(Note o1, Note o2) {
+                return (int)(o1.date - o2.date);
+            }
+
+            @Override
+            public void onChanged(int position, int count) {
+                notifyItemRangeChanged(position, count);
+            }
+
+            @Override
+            public boolean areContentsTheSame(Note oldItem, Note newItem) {
+                return oldItem.equals(newItem);
+            }
+
+            @Override
+            public boolean areItemsTheSame(Note item1, Note item2) {
+                return item1.uid == item2.uid;
+            }
+
+            @Override
+            public void onInserted(int position, int count) {
+                notifyItemRangeInserted(position, count);
+            }
+
+            @Override
+            public void onRemoved(int position, int count) {
+                notifyItemRangeRemoved(position, count);
+            }
+
+            @Override
+            public void onMoved(int fromPosition, int toPosition) {
+                notifyItemMoved(fromPosition, toPosition);
+            }
+        });
+    }
+
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new NoteViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_note_list, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
+        holder.bind(sortedList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return sortedList.size();
+    }
+
+    public void setItems(List<Note> notes)
+    {
+        sortedList.replaceAll(notes);
+    }
+
+    static class NoteViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView noteTitle;
+
+            Note note;
+
+        public NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            noteTitle = itemView.findViewById(R.id.note_title);
+        }
+
+        public void bind(Note note)
+        {
+            this.note = note;
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/text_notes_app/screens/main/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.text_notes_app.screens.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.os.RecoverySystem;
+import android.view.View;
+import android.widget.Toolbar;
+
+import com.example.text_notes_app.R;
+import com.example.text_notes_app.model.Note;
+import com.example.text_notes_app.screens.details.NoteDetailsActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        recyclerView = findViewById(R.id.list);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+
+        Adapter adapter = new Adapter();
+        recyclerView.setAdapter(adapter);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NoteDetailsActivity.start(MainActivity.this, null);
+            }
+        });
+
+        MainViewModel mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        mainViewModel.getNoteLiveData().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                adapter.setItems(notes);
+            }
+        });
+    }
+
+    private void setSupportActionBar(Toolbar toolbar) {
+    }
+}
